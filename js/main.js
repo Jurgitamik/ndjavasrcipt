23fc5a94637f292a976591c1b088a5089c97fd77
Jurgitamik <56818575+Jurgitamik@users.noreply.github.com>
2019-11-05 13:40:55 +0200

    kintamuju inicijavimas

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Kintamuju incijavimas
+//1.
+const du = ( 2 );
+console.log(du);
+
+const grozis = (6);
+console.log(grozis);
+
+const stuka = (1000);
+console.log(stuka);
+
+//2.
+const namas = ('namas');
+console.log(namas);
+
+const pirkia = ('namas');
+console.log(pirkia);
+
+const troba = ('namas');
+console.log(troba);
+
+//3.
+const list = [8, 7, 6, 5, 4];
+console.log(list);
+
+const listas = [3.5, 5.5, 7.5, 9.5, 1.5];
+console.log(listas);
+
+const listukas = [2, 3, 2, 3, 2];
+console.log(listukas);
+
+//4. 
+const sarasas = ['as', 'tu', 'jis', 'ji', 'jie'];
+console.log(sarasas);
+
+const balses = ['a', 'e', 'o', 'i', 'u'];
+console.log(balses);
+
+const priebalses = ['r', 't', 'p', 's', 'd'];
+console.log(priebalses);
+
+
+
+
+
+
+
+
+
+
+
